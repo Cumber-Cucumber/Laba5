a73527f1645f251cfa560b5921998ce5ffc1dc77
Cumber Cucumber <Wanjawaluj111@gmail.com>
2020-10-28 10:49:17 +0300

    omg one commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static Laba5.laba5;
+
+namespace Laba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Checkbox checkbox1 = new Checkbox(10, 8, "blue", true);
+            checkbox1.Show();
+            checkbox1.Input(5);
+            checkbox1.Show();
+            Console.WriteLine($"Ширина: {checkbox1.Width}, высота: {checkbox1.Height}");
+            Checkbox checkbox2 = new Checkbox(10, 10, "darkblue", true);
+            checkbox2.Show();
+
+            Console.WriteLine("----------------------------------------------------\n");
+
+            Radiobutton radiobutton1 = new Radiobutton(10, "blue", true);
+            radiobutton1.Input(15);
+            radiobutton1.Diameter = 15;
+            radiobutton1.Show();
+
+            Console.WriteLine("----------------------------------------------------\n");
+
+            Button button1 = new Button(false);
+            button1.Show();
+
+            Console.WriteLine("----------------------------------------------------\n");
+
+            Checkbox checkbox3 = new Checkbox(4, 4, "red", false);
+            Button button_ch3 = checkbox3 as Button; // Апкастим/работаем со ссылкой
+            Control_Element cont_el1 = new Button(true);
+            cont_el1.Show();
+            if (cont_el1 is IControl)
+            {
+                IControl cont_el2 = cont_el1;
+                cont_el2.Show();
+            }
+
+            Console.WriteLine("----------------------------------------------------\n");
+
+            Control_Element[] arr = { checkbox1, checkbox2, radiobutton1, button1, cont_el1 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Printer.iAmPrinting(arr[i]);
+            }
+        }
+    }
+}
---
+++ laba5.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Laba5
+{
+    class laba5
+    {
+        sealed class NONONO
+        {
+
+        }
+        interface IGeometric_Figure
+        {
+            string Color { get; }
+        }
+        interface ICircle : IGeometric_Figure
+        {
+            int Diameter { get; set; }
+        }
+        interface IRectangle : IGeometric_Figure
+        {
+            int Height { get; }
+            int Width { get; }
+        }
+        public interface IControl
+        {
+            void Show();
+            void Input(int size);
+            void Resize(int size);
+        }
+
+        public abstract class Control_Element : IControl
+        {
+            protected int size;
+            public virtual void Show()
+            {
+                Console.WriteLine("Если это сообщение видно, значит в дочернем классе не реализован метод Scow() или метод вызывается из базового (этого) класса");
+            }
+            public void Input(int _size)
+            {
+                size += _size;
+            }
+            public void Resize(int _size)
+            {
+                size = _size;
+            }
+
+        }
+        public class Button : Control_Element
+        {
+            public bool Clickable { get; set; }
+
+            public Button(bool _clicable)
+            {
+                Clickable = _clicable;
+            }
+            new public void Show()
+            {
+                Console.WriteLine("Сведения о button:");
+                Console.WriteLine($"Изменяемый параметр: {size}, кликабельность: {Clickable}");
+            }
+
+            public override bool Equals(object obj) // Переопределение
+            {
+                if (obj.GetType() == this.GetType()) // Преколы с получением типа объекта и сравнения его с типом проверяемого объекта (текущего)
+                    return true;
+                else
+                    return false;
+            }
+            public override string ToString() // Переопределение
+            {
+                Console.WriteLine("Сведения о button:");
+                Console.WriteLine($"Изменяемый параметр: {size}, кликабельность: {Clickable}");
+                return "";
+            }
+            public int gethashcode_class_parameter;
+            public override int GetHashCode() // Переопределение
+            {
+                return gethashcode_class_parameter * gethashcode_class_parameter / 8;
+            }
+        }
+        public class Checkbox : Button, IRectangle, IControl
+        {
+            public int Height { get; }
+            public int Width { get; }
+            public string Color { get; }
+
+            public Checkbox(int _height, int _width, string _color, bool __clicable) : base(__clicable)
+            {
+                Height = _height;
+                Width = _width;
+                Color = _color;
+            }
+
+            new public void Show()
+            {
+                Console.WriteLine("Сведения о checkbox:");
+                Console.WriteLine($"Изменяемый параметр: {size}, цвет: {Color}, величины: {Width}x{Height}");
+            }
+
+            public override string ToString() // Переопределение
+            {
+                Console.WriteLine("Сведения о checkbox:");
+                Console.WriteLine($"Изменяемый параметр: {size}, цвет: {Color}, величины: {Width}x{Height}");
+                return "";
+            }
+        }
+        public class Radiobutton : Button, ICircle, IControl
+        {
+            public int Diameter { get; set; }
+            public string Color { get; }
+
+            public Radiobutton(int _diameter, string _color, bool __clicable) : base(__clicable)
+            {
+                Diameter = _diameter;
+                Color = _color;
+            }
+
+            new public void Show()
+            {
+                Console.WriteLine("Сведения о radiobutton:");
+                Console.WriteLine($"Изменяемый параметр: {size}, цвет: {Color}, диаметр: {Diameter}");
+            }
+
+            public override string ToString() // Переопределение
+            {
+                Console.WriteLine("Сведения о radiobutton:");
+                Console.WriteLine($"Изменяемый параметр: {size}, цвет: {Color}, диаметр: {Diameter}");
+                return "";
+            }
+        }
+
+
+        public class Printer
+        {
+            public static void iAmPrinting(Control_Element someobj)
+            {
+                Console.WriteLine(someobj.GetType());
+                Console.WriteLine(someobj.ToString());
+            }
+        }
+
+    }
+}
